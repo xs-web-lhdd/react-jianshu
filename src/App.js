93e6034778c5e025641d2f9f1089449e1fcca143
凉风有信 <3042146237@qq.com>
2021-08-25 16:41:44 +0800

    feat:项目初始化

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyle } from './style.js';
+
+function App() {
+  return (
+    <div className='App'>
+      hello world
+      <GlobalStyle/>
+    </div>
+  )
+}
+
+export default App;
